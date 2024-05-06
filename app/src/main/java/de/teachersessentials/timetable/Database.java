6cba125d0724d184915687bb8e0616c8fb0b75aa
Alex <alex.meyer.2007@web.de>
2024-05-06 15:08:35 +0200

    csv table output management

--- app/src/main/java/de/teachersessentials/timetable/Database.java
+++ app/src/main/java/de/teachersessentials/timetable/Database.java
@@ -1,11 +1,16 @@
 package de.teachersessentials.timetable;
 
+import android.content.Context;
+import android.graphics.Color;
+
 import java.util.ArrayList;
+
+import javax.security.auth.Subject;
 
 import de.teachersessentials.csv.CsvParser;
 
 public class Database {
-    public static void load() {
+    public static void load(Context context) {
         //private void loadSubjects() {
         //
         //}
@@ -16,18 +21,55 @@ public class Database {
         //
         //}
 
-//        ArrayList<String[]> rawLessons = CsvParser.read("file");
-//        ArrayList<Lesson> lessons = new ArrayList<>();
-//        for (String[] item : rawLessons) {
-//
-//            int id = Integer.parseInt(item[0]);
-//            int day = Integer.parseInt(item[1]);
-//            int time = Integer.parseInt(item[2]);
-//            int subject = Integer.parseInt(item[3]);
-//            int class_ = Integer.parseInt(item[4]);
-//            int room = Integer.parseInt(item[5]);
-//            //Lesson lesson = new Lesson(id, day, lesson, subject, class_, room);
-//        }
+        ArrayList<String[]> rawLessons = CsvParser.read("timetable",context);
+        ArrayList<Lesson> lessons = new ArrayList<>();
+        for (String[] item : rawLessons) {
+
+            int id = Integer.parseInt(item[0]);
+            int day = Integer.parseInt(item[1]);
+            int time = Integer.parseInt(item[2]);
+            int subject = Integer.parseInt(item[3]);
+            int class_ = Integer.parseInt(item[4]);
+            int room = Integer.parseInt(item[5]);
+
+            Lesson lesson = new Lesson(id, day, time, subject, class_, room);
+            lessons.add(lesson);
+        }
+
+        ArrayList<String[]> rawSubjects = CsvParser.read("subjects",context);
+        ArrayList<TimetableSubject> subjects = new ArrayList<>();
+        for (String[] item : rawSubjects) {
+
+            int id = Integer.parseInt(item[0]);
+            String display_name = item[1];
+            String name = item[2];
+            Color color = Integer.parseInt(item[3]);
+
+            TimetableSubject subject = new TimetableSubject(id, display_name, name, color);
+            subjects.add(subject);
+        }
+
+        ArrayList<String[]> rawRooms = CsvParser.read("room",context);
+        ArrayList<Room> rooms = new ArrayList<>();
+        for (String[] item : rawRooms) {
+
+            int id = Integer.parseInt(item[0]);
+            String name = item[1];
+
+            Room room = new TimetableClass(id, name);
+            rooms.add(room);
+        }
+
+        ArrayList<String[]> rawClasses = CsvParser.read("class",context);
+        ArrayList<TimetableClass> classes = new ArrayList<>();
+        for (String[] item : rawClasses) {
+
+            int id = Integer.parseInt(item[0]);
+            String name = item[1];
+
+            TimetableClass class_ = new TimetableClass(id, name);
+            classes.add(class_);
+        }
         
     }
     public static void save() {
