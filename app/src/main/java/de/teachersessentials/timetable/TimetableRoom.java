761bfa16f92930efa85425e0da64d76108d7c8fd
Stefan <stefan.koellner7@gmail.com>
2024-05-06 15:06:43 +0200

    Klasse Room schöner aussehend gemacht :DDD

--- app/src/main/java/de/teachersessentials/timetable/TimetableRoom.java
+++ app/src/main/java/de/teachersessentials/timetable/TimetableRoom.java
@@ -2,7 +2,7 @@ package de.teachersessentials.timetable;
 
 import android.graphics.Color;
 
-public class Room {
+public class TimetableRoom {
     Room(int id_, int room_) {
 
         int id = id_;
