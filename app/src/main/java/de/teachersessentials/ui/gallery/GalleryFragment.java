565ace6a9721dc37b8b04f8d260417d1d0fdbab9
Alex <alex.meyer.2007@web.de>
2024-05-06 14:06:10 +0200

    csv debugging

--- app/src/main/java/de/teachersessentials/ui/gallery/GalleryFragment.java
+++ app/src/main/java/de/teachersessentials/ui/gallery/GalleryFragment.java
@@ -1,15 +1,21 @@
 package de.teachersessentials.ui.gallery;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.util.ArrayList;
+
+import de.teachersessentials.R;
+import de.teachersessentials.csv.CsvParser;
 import de.teachersessentials.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends Fragment {
@@ -24,8 +30,41 @@ public class GalleryFragment extends Fragment {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
+        final TextView textView = binding.textViewTest;
         galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        Button testbutton = (Button) root.findViewById(R.id.button_test);
+        testbutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                TextView textView_test = (TextView) root.findViewById(R.id.textView_test);
+                ArrayList<String[]> data = new ArrayList<>();
+                String[] line = new String[4];
+                line[0] = "hallo";
+                line[1] = "test";
+                line[2] = "hier";
+                line[3] = "CSV";
+                data.add(line);
+
+                String[] headers = new String[1];
+                headers[0] = "test";
+
+
+                CsvParser.save("test.csv", data, headers, "Testtabelle", getActivity().getApplicationContext());
+                ArrayList<String[]> loadedData = CsvParser.read("test.csv", getActivity().getApplicationContext());
+
+
+                textView_test.setText(loadedData.get(0).toString());
+                String text = new String();
+                for (String s : loadedData.get(0) )
+                {
+                    text += s;
+                }
+                textView_test.setText(text);
+            }
+        });
+
         return root;
     }
 
@@ -34,4 +73,6 @@ public class GalleryFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+
 }
