a08b972a1d1ade7d7afaa033d215ec33b327ee15
Volvo7700 <roman.dubitzky@gmail.com>
2024-05-23 16:50:06 +0200

    Convert Color to int

--- app/src/main/java/de/teachersessentials/timetable/TimetableSubject.java
+++ app/src/main/java/de/teachersessentials/timetable/TimetableSubject.java
@@ -3,11 +3,10 @@ package de.teachersessentials.timetable;
 import android.graphics.Color;
 
 public class TimetableSubject {
-    TimetableSubject(int id_,String shortage_,String name_,Color color_) {
+    TimetableSubject(int id_,String shortage_,String name_,int color_) {
     int id = id_;
     String shortage = shortage_;
     String name = name_;
-    Color color = color_;
-
+    int color = color_;
     }
 }
