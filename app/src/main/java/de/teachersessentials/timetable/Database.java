18e008c5ee31799e6600bba27ae93908fac1271d
Volvo7700 <roman.dubitzky@gmail.com>
2024-06-12 17:39:03 +0200

    Methode zur Datenbankfütterung mit Standarddaten hinzugefügt

--- app/src/main/java/de/teachersessentials/timetable/Database.java
+++ app/src/main/java/de/teachersessentials/timetable/Database.java
@@ -129,4 +129,67 @@ public class Database {
 
 
     }
+
+    // Die Datenbank mit den aktuellen Standarddaten füttern
+    public static void generateDefaults(Context context) {
+        String[] classes = { "5A", "5B", "5C", "5D", "5E", "5F", "5G", "5H", "5I",
+                "6A", "6B", "6C", "6D", "6E", "6F", "6H", "6I", "6K",
+                "7A", "7B", "7C", "7D", "7E", "7F", "7H",
+                "8A", "8B", "8C", "8D", "8E", "8F",
+                "9A", "9B", "9C", "9D", "9E", "9F",
+                "10A", "10B", "10C", "10D", "10E", "10F", "10G",
+                "11A", "11B", "11C", "11D", "11E", "11F",
+                "12Q1", "12Q2", "12Q3", "12Q4", "12Q5", "12Q6" };
+
+        String[] rooms = { "010", "014", "016", "018", "019", "021", "022", "025", "027", "028", "029", "040"," 042"," 044", "045", "046", "047", "049", "053", "060", "069", "084",
+                "111", "112", "113", "117", "118", "121", "122", "123", "124", "125", "126", "127", "131", "132", "133", "134", "135", "136", "137", "141", "142", "143", "144", "145", "146", "147",
+                "211", "212", "213", "214", "215", "216", "217", "221", "222", "223", "224", "225", "226", "231", "232", "233", "234", "235", "241", "242", "243", "242", "245", "246",
+                "S004", "S005", "S007", "S008", "S009", "S010", "S011", "S013", "S014", "SU13", "SU14", "SU15",
+                "S104", "S105", "S107", "S108", "S109", "S110", "S111",
+                "S204", "S205", "S206", "S207", "S208", "S209", "S210", "S211", "S212", "S213", "S215", "S218",
+                "S304", "S305", "S307", "S308", "S313", "S314" };
+
+        String[] subjects = { "Deutsch", "Mathematik",
+                "Englisch", "Französisch", "Latein", "Spanisch",
+                "Physik", "Chemie", "Biologie", "Informatik",
+                "Katholisch", "Evangelisch", "Ethik",
+                "Geographie", "Geschichte", "Politik und Gesellschaft",
+                "Sport", "Kunst", "Musik",
+                "Physik Übung", "Chemie Übung",
+                "Deutsch Int.", "Mathematik Int.", "Französisch Int.", "Latein Int.",
+                "P-Seminar", "Modul zur berufl. Orientierung" };
+
+        Integer[] colors = { 0xD70000, 0x0080FF,
+                0x00CC00, 0xFFFF00, 0x990000, 0x00CC66,
+                0x0000FF, 0xFF00FF, 0x99FF33, 0x00FFFF,
+                0x9933FF, 0x9933FF, 0x9933FF,
+                0x994C00, 0x808080, 0xFF8000,
+                0xE0E0E0, 0x009999, 0xCC0066,
+                0x0000FF, 0xFFFF00,
+                0xD70000, 0x0080FF, 0xFFFF00, 0x990000,
+                0x000000, 0x404040
+        };
+
+        ArrayList<String[]> classes_arr = new ArrayList<>();
+        ArrayList<String[]> rooms_arr = new ArrayList<>();
+        ArrayList<String[]> subjects_arr = new ArrayList<>();
+
+        // Arrays jeweils durchgehen und mit ins passende Format (Arraylist<String> mit "Primärschlüssel") konvertieren
+        for (int i = 0; i < classes.length; i++) {
+            String[] row = { Integer.toString(i), classes[i] };
+            classes_arr.add(row);
+        }
+        for (int i = 0; i < rooms.length; i++) {
+            String[] row = { Integer.toString(i), rooms[i] };
+            rooms_arr.add(row);
+        }
+        for (int i = 0; i < subjects.length; i++) {
+            String[] row = { Integer.toString(i), subjects[i], colors[i].toString() };
+            subjects_arr.add(row);
+        }
+
+        CsvParser.write("classes.csv", classes_arr, new String[] {"ID", "Klasse"}, "Klassen", context);
+        CsvParser.write("rooms.csv", rooms_arr, new String[] {"ID", "Raum"}, "Räume", context);
+        CsvParser.write("subjects.csv", subjects_arr, new String[] {"ID", "Fach"}, "Fächer", context);
+    }
 }
