5d181aa545c8d8da761771a53179c85f246d34a5
MuffinHD <125144775+MuffinHD@users.noreply.github.com>
2024-06-14 20:14:43 +0200

    Layout von eingabe neuer Sachen

--- app/src/main/java/de/teachersessentials/ui/gallery/PopUpAdd.java
+++ app/src/main/java/de/teachersessentials/ui/gallery/PopUpAdd.java
@@ -5,8 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import de.teachersessentials.R;
 import de.teachersessentials.timetable.Timetable;
@@ -30,30 +36,49 @@ public class PopUpAdd extends Activity {
         TextView head = findViewById(R.id.head);
 
         EditText textAdd = findViewById(R.id.text_add);
-        textAdd.setMinimumWidth(600);
 
         TextView error = findViewById(R.id.error_input);
+
+        Button cancel = findViewById(R.id.cancel);
+        cancel.setOnClickListener(v -> finish());
 
         Button save = findViewById(R.id.save);
 
         //TODO: fertig machen, wenn vom Database team gefixt
         if (addId == addButtonIds[0]) { //Fach hinzufügen
             head.setText("Fach Hinzufügen");
-            Spinner spinner = findViewById(R.id.select_color);
-            spinner.setVisibility(View.VISIBLE);
-            spinner.setMinimumWidth(600);
+            textAdd.setHint("Fach");
+            EditText addShortage = findViewById(R.id.shortage_add);
+
+            RelativeLayout relativeLayout2 = findViewById(R.id.relativ_layout_2);
+            relativeLayout2.setVisibility(View.VISIBLE);
+
+            error.setVisibility(View.GONE);
+
+            TextView colorsText = findViewById(R.id.colors_text);
+            colorsText.setVisibility(View.VISIBLE);
+
+            ArrayList<Button> color_buttons = new ArrayList<>();
+            for (int n = 1; n <= 16; n += 1) {
+                int id = getResources().getIdentifier("color_" + (n), "id", getPackageName());
+                Button button = findViewById(id);
+                button.setVisibility(View.VISIBLE);
+            }
 
             save.setOnClickListener(v -> {
                 String newSubject = String.valueOf(textAdd.getText());
+                String shortage = String.valueOf(addShortage.getText());
                 if (newSubject.isEmpty()) {
+                    error.setVisibility(View.VISIBLE);
                     error.setText("Bitte Name des Fachs eingeben");
                 } else {
-                    Timetable.setSubject(newSubject, "", 1); //Placeholder für shortage und color
+                    Timetable.setSubject(newSubject, shortage, 1); //Placeholder für color
                     finish();
                 }
             });
         } else if (addId == addButtonIds[1]) { //Raum hinzufügen
             head.setText("Raum Hinzufügen");
+            textAdd.setHint("Raum");
 
             save.setOnClickListener(v -> {
                 String newRoom = String.valueOf(textAdd.getText());
@@ -66,6 +91,7 @@ public class PopUpAdd extends Activity {
             });
         } else { //Klasse hinzufügen
             head.setText("Klasse hinzufügen");
+            textAdd.setHint("Klasse");
 
             save.setOnClickListener(v -> {
                 String newClass = String.valueOf(textAdd.getText());
