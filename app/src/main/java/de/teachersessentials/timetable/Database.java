ed4d7d50530dc1a6fb7e209cd31cbee050957b6a
Roman <roman.dubitzky@gmail.com>
2024-06-23 20:41:48 +0200

    Schwerwiegenden Speicherfehler behoben und Errorhandling hinzugefügt

--- app/src/main/java/de/teachersessentials/timetable/Database.java
+++ app/src/main/java/de/teachersessentials/timetable/Database.java
@@ -13,115 +13,173 @@ public class Database {
     public static ArrayList<TimetableClass> classes = new ArrayList<>();
 
 
-    public static void load(Context context) {
+    public static boolean load(Context context) {
 
         ///// liest die CSV Dateien ("timetable.csv","subjects.csv","room.csv","class.csv") von dem mobilen Endgerät aus,
         ///// konvertiert sie von String zu ihren gewünschten Datentypen
         ///// und speichert sie von ArrayList<String[]> in Variablen der Objekte Lesson, TimetableSubject, TimetableRoom, TimetableClass
 
-        ArrayList<String[]> rawLessons = CsvParser.read("timetable.csv",context);
-        for (String[] item : rawLessons) {
+        // Fehlervariable: Wenn Fehler auftreten, wird sie auf true gesetzt, damit man eine Fehlermeldung anzeigen kann
+        boolean error = false;
 
-            int id = Integer.parseInt(item[0]);
-            int day = Integer.parseInt(item[1]);
-            int time = Integer.parseInt(item[2]);
-            int subject = Integer.parseInt(item[3]);
-            int class_ = Integer.parseInt(item[4]);
-            int room = Integer.parseInt(item[5]);
+        try {
+            ArrayList<String[]> rawLessons = CsvParser.read("timetable.csv",context);
+            for (String[] item : rawLessons) {
+                try {
+                    int id = Integer.parseInt(item[0]);
+                    int day = Integer.parseInt(item[1]);
+                    int time = Integer.parseInt(item[2]);
+                    int subject = Integer.parseInt(item[3]);
+                    int class_ = Integer.parseInt(item[4]);
+                    int room = Integer.parseInt(item[5]);
 
-            Lesson lesson = new Lesson(id, day, time, subject, class_, room);
-            lessons.add(lesson);
+                    Lesson lesson = new Lesson(id, day, time, subject, class_, room);
+                    lessons.add(lesson);
+                }
+                catch (Exception ex) {
+                    error = true;
+                }
+            }
+        }
+        catch (Exception ex) {
+            error = true;
         }
 
-        ArrayList<String[]> rawSubjects = CsvParser.read("subjects.csv",context);
-        for (String[] item : rawSubjects) {
+        try {
+            ArrayList<String[]> rawSubjects = CsvParser.read("subjects.csv",context);
+            for (String[] item : rawSubjects) {
+                try {
+                    int id = Integer.parseInt(item[0]);
+                    String display_name = item[1];
+                    String name = item[2];
+                    int colorint = Integer.parseInt(item[3]);
 
-            int id = Integer.parseInt(item[0]);
-            String display_name = item[1];
-            String name = item[2];
-            int colorint = Integer.parseInt(item[3]);
-
-            TimetableSubject subject = new TimetableSubject(id, display_name, name, colorint);
-            subjects.add(subject);
+                    TimetableSubject subject = new TimetableSubject(id, display_name, name, colorint);
+                    subjects.add(subject);
+                }
+                catch (Exception ex) {
+                    error = true;
+                }
+            }
+        }
+        catch (Exception ex) {
+            error = true;
         }
 
-        ArrayList<String[]> rawRooms = CsvParser.read("rooms.csv",context);
-        for (String[] item : rawRooms) {
+        try {
+            ArrayList<String[]> rawRooms = CsvParser.read("rooms.csv",context);
+            for (String[] item : rawRooms) {
+                try {
+                    int id = Integer.parseInt(item[0]);
+                    String name = item[1];
 
-            int id = Integer.parseInt(item[0]);
-            String name = item[1];
-
-            TimetableRoom room = new TimetableRoom(id, name);
-            rooms.add(room);
+                    TimetableRoom room = new TimetableRoom(id, name);
+                    rooms.add(room);
+                }
+                catch (Exception ex) {
+                    error = true;
+                }
+            }
+        }
+        catch (Exception ex) {
+            error = true;
         }
 
-        ArrayList<String[]> rawClasses = CsvParser.read("classes.csv",context);
-        for (String[] item : rawClasses) {
+        try {
+            ArrayList<String[]> rawClasses = CsvParser.read("classes.csv", context);
+            for (String[] item : rawClasses) {
+                try {
+                    int id = Integer.parseInt(item[0]);
+                    String name = item[1];
 
-            int id = Integer.parseInt(item[0]);
-            String name = item[1];
-
-            TimetableClass class_ = new TimetableClass(id, name);
-            classes.add(class_);
+                    TimetableClass class_ = new TimetableClass(id, name);
+                    classes.add(class_);
+                }
+                catch (Exception ex) {
+                    error = true;
+                }
+            }
         }
-        
+        catch (Exception ex) {
+            error = true;
+        }
+        return error;
     }
-    public static void save(Context context) {
+    public static boolean save(Context context) {
 
         /////// liest die Variablen der Objekte Lesson, TimetableSubject, TimetableRoom und TimetableClass
         /////// und speichert sie in einer ArrayList<String[]>   ( 1 Liste pro Objekt )
         /////// diese ArrayLists werden in ihren Dateien auf dem digitalen Endgerät mithilfe der Metode CsvParser.save() gespeichert
 
-        String[] headers = {"id","day","hour","subject_id","class_id","room_id"};
-        ArrayList<String[]> rawLessons = new ArrayList<>();
-        for (Lesson lesson : lessons){
-            String[] values = new String[6];
-            values[0] = (String.valueOf(lesson.id));
-            values[1] = (String.valueOf(lesson.day));
-            values[2] = (String.valueOf(lesson.hour));
-            values[3] = (String.valueOf(lesson.subject));
-            values[4] = (String.valueOf(lesson.class_));
-            values[5] = (String.valueOf(lesson.room));
-            rawLessons.add(values);
+        boolean error = false;
+
+        try {
+            String[] headers = {"id","day","hour","subject_id","class_id","room_id"};
+            ArrayList<String[]> rawLessons = new ArrayList<>();
+            for (Lesson lesson : lessons){
+                String[] values = new String[6];
+                values[0] = (String.valueOf(lesson.id));
+                values[1] = (String.valueOf(lesson.day));
+                values[2] = (String.valueOf(lesson.hour));
+                values[3] = (String.valueOf(lesson.subject));
+                values[4] = (String.valueOf(lesson.class_));
+                values[5] = (String.valueOf(lesson.room));
+                rawLessons.add(values);
+            }
+            CsvParser.write("timetable.csv",rawLessons,headers,"Stundenplan",context);
         }
-        CsvParser.write("timetable.csv",rawLessons,headers,"Stundenplan",context);
-
-
-        String[] headers2 = {"id","shortage","name","color"};
-        ArrayList<String[]> rawSubjects = new ArrayList<>();
-        for (TimetableSubject subject : subjects){
-            String[] values = new String[4];
-            values[0] = (String.valueOf(subject.id));
-            values[1] = subject.shortage;
-            values[2] = subject.name;
-            values[3] = (String.valueOf(subject.color));
-            rawLessons.add(values);
+        catch (Exception ex) {
+            error = true;
         }
-        CsvParser.write("subjects.csv",rawSubjects,headers2,"Fächer",context);
 
-
-        String[] headers3 = {"id","room_name"};
-        ArrayList<String[]> rawRooms = new ArrayList<>();
-        for (TimetableRoom room : rooms){
-            String[] values = new String[2];
-            values[0] = (String.valueOf(room.id));
-            values[1] = room.room;
-            rawLessons.add(values);
+        try {
+            String[] headers2 = {"id","shortage","name","color"};
+            ArrayList<String[]> rawSubjects = new ArrayList<>();
+            for (TimetableSubject subject : subjects){
+                String[] values = new String[4];
+                values[0] = (String.valueOf(subject.id));
+                values[1] = subject.shortage;
+                values[2] = subject.name;
+                values[3] = (String.valueOf(subject.color));
+                rawSubjects.add(values);
+            }
+            CsvParser.write("subjects.csv",rawSubjects,headers2,"Fächer",context);
         }
-        CsvParser.write("rooms.csv",rawRooms,headers3,"Räume",context);
-
-
-        String[] headers4 = {"id","class_name"};
-        ArrayList<String[]> rawClasses = new ArrayList<>();
-        for (TimetableClass class_ : classes){
-            String[] values = new String[2];
-            values[0] = (String.valueOf(class_.id));
-            values[1] = class_.name;
-            rawLessons.add(values);
+        catch (Exception ex) {
+            error = true;
         }
-        CsvParser.write("classes.csv",rawClasses,headers4,"Klassen",context);
 
+        try {
+            String[] headers3 = {"id","room_name"};
+            ArrayList<String[]> rawRooms = new ArrayList<>();
+            for (TimetableRoom room : rooms){
+                String[] values = new String[2];
+                values[0] = (String.valueOf(room.id));
+                values[1] = room.room;
+                rawRooms.add(values);
+            }
+            CsvParser.write("rooms.csv",rawRooms,headers3,"Räume",context);
+        }
+        catch (Exception ex) {
+            error = true;
+        }
 
+        try {
+            String[] headers4 = {"id","class_name"};
+            ArrayList<String[]> rawClasses = new ArrayList<>();
+            for (TimetableClass class_ : classes){
+                String[] values = new String[2];
+                values[0] = (String.valueOf(class_.id));
+                values[1] = class_.name;
+                rawClasses.add(values);
+            }
+            CsvParser.write("classes.csv",rawClasses,headers4,"Klassen",context);
+        }
+        catch (Exception ex){
+            error = true;
+        }
+
+        return error;
     }
 
     // Die Datenbank mit den aktuellen Standarddaten füttern
@@ -195,5 +253,6 @@ public class Database {
         CsvParser.write("classes.csv", classes_arr, new String[] {"ID", "Klasse"}, "Klassen", context);
         CsvParser.write("rooms.csv", rooms_arr, new String[] {"ID", "Raum"}, "Räume", context);
         CsvParser.write("subjects.csv", subjects_arr, new String[] {"ID", "Fach"}, "Fächer", context);
+        Database.load(context);
     }
 }
